34de54fa310d28dfe8e09bc55cd024b0c3d0ec8a
Kano <kan0000413@gmail.com>
2025-03-17 17:33:20 +0530

    Removed email field from User model to fix duplicate key error

--- models/User.js
+++ models/User.js
@@ -1,27 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  mobile: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  profilePicture: {
-    type: String,
-    default: ''
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  name: { type: String, required: true },
+  mobile: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePicture: { type: String }
 });
 
 module.exports = mongoose.model('User', userSchema);
